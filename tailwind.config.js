b873eb498506e21d2e384f39cf1365f41cb78bc2
adnan <katmanfatahoni@gmail.com>
2024-05-06 23:13:36 +0700

    flex and row

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    borderWidth: {
+      DEFAULT: "1px",
+      0: "0",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      6: "6px",
+      8: "8px",
+      9: "50px",
+    },
   },
   plugins: [],
 };
